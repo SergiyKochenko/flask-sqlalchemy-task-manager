49fd1e6cae08c4dcf1c5dddf4fc15ffbd0827411
SergiyKochenko <sergiykochenko@gmail.com>
2023-02-17 13:52:52 +0000

    fix bug

--- env_sample.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "any_secret_key")
-os.environ.setdefault("DEBUG", "True")
-os.environ.setdefault("DEVELOPMENT", "True")
-os.environ.setdefault("DB_URL", "postgresql:///taskmanager")
